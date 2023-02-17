279d771a4702f2e3cd35e10958f2fdec2dcc475e
SandipSolunke <solunkesandip39@gmail.com>
2023-02-17 14:21:15 +0530

    fixed publish pagae

--- frontend/src/Components/CreatorComponents/Publish/Publish.js
+++ frontend/src/Components/CreatorComponents/Publish/Publish.js
@@ -1,31 +1,29 @@
-import React from 'react'
+import React from "react";
 import Navbar from "../Navbar/Navbar";
-import { useLocation,useParams,useNavigate } from "react-router-dom";
-
-
-const Publish=(props)=>{
-    const navigate=useNavigate();
-    const location=useLocation();
-    const {email}=useParams();
-
-    const hostname=window.location.hostname
-    const port=window.location.port
-    const formID=location.state.formID
-    const url=`http://${hostname}:${port}/userend/${formID}`;
-
-    return(
-        <div>
-            <Navbar email={email}/>
-            <h1>Publish Form</h1>
-            <div>
-                <h2>Generated Link :</h2>
-
-                <h3 style={{color:"blue",cursor:"pointer" }} >{url}</h3>
-                <button onClick={() => { navigator.clipboard.writeText(url) }}>Copy to clipboard</button>
+import { useLocation, useParams, useNavigate } from "react-router-dom";
+import "./Publish.css";
+const Publish = (props) => {
+    const navigate = useNavigate();
+    const location = useLocation();
+    const { email } = useParams();
+    const hostname = window.location.hostname;
+    const port = window.location.port;
+    const formID = location.state.formID;
+    const url = `http://${hostname}:${port}/userend/${formID}`;
+    return (
+        <div>      <Navbar email={email} />
+            <div className="publish-form-root">
+                <h1 className="publish-margin publish-heading">Published!</h1>
+                <div>
+                    <h2 className="publish-margin">Generated Link :</h2>
+                    <h3 className="publish-margin" style={{ color: "blue", cursor: "pointer" }}>{url}</h3>
+                    <button className="publish-margin  publish-btn" onClick={() => {
+                        navigator.clipboard.writeText(url);
+                    }}>
+                        Copy to clipboard
+                    </button>
+                </div>
             </div>
-        </div>
-    )
-}
-
-
+        </div>);
+};
 export default Publish;
