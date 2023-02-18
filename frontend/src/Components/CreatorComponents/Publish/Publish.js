c8c535fef0be1ce5309c3909e4bb173c33ba3546
SandipSolunke <solunkesandip39@gmail.com>
2023-02-18 12:08:03 +0530

    ntohing

--- frontend/src/Components/CreatorComponents/Publish/Publish.js
+++ frontend/src/Components/CreatorComponents/Publish/Publish.js
@@ -1,15 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Navbar from "../Navbar/Navbar";
 import { useLocation, useParams, useNavigate } from "react-router-dom";
 import "./Publish.css";
 const Publish = (props) => {
-    const navigate = useNavigate();
     const location = useLocation();
     const { email } = useParams();
+    const [formID, setFormID] = useState("");
+    const [url, setUrl] = useState("");
+
     const hostname = window.location.hostname;
     const port = window.location.port;
-    const formID = location.state.formID;
-    const url = `http://${hostname}:${port}/userend/${formID}`;
+
+
+    useEffect(() => {
+        setUrl(`http://${hostname}:${port}/userend/${formID}`);
+    }, [formID])
+
+    useEffect(() => {
+        setFormID(location.state.formID)
+    }, [])
+    // const formID = location.state.formID;
+    // const url = `http://${hostname}:${port}/userend/${formID}`;
     return (
         <div>      <Navbar email={email} />
             <div className="publish-form-root">
